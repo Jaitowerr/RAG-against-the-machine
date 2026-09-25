ea29d4c6d69099d5e9ab4f0f3032df9ad64e4a3a
Jaitowerr <207956068+Jaitowerr@users.noreply.github.com>
2026-09-25 13:24:37 +0200

    variable set-local para 42, cache en sgoinfree

---
+++ set-local.sh
@@ -0,0 +1,15 @@
+export CALLME_STORAGE="/home/aitorres/sgoinfre/RAG"
+
+export UV_CACHE_DIR="$CALLME_STORAGE/uv-cache"
+export UV_PROJECT_ENVIRONMENT="$CALLME_STORAGE/venv"
+export UV_PYTHON_INSTALL_DIR="$CALLME_STORAGE/python"
+
+export HF_HOME="$CALLME_STORAGE/huggingface"
+export HF_HUB_CACHE="$HF_HOME/hub"
+
+export TMPDIR="$CALLME_STORAGE/tmp"
+export XDG_CACHE_HOME="$CALLME_STORAGE/xdg-cache"
+
+
+# uv cache dir
+# source set-local.sh
